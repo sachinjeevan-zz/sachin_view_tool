2bb672316f4aa1cb5715d335ba9c5f1fe8099be7
sachinjeevan <sachinjeevan2506@gmail.com>
2018-03-23 23:23:33 +0530

    view_tool

---
+++ lib/sachin_view_tool.rb
@@ -0,0 +1,5 @@
+require "sachin_view_tool/version"
+require "sachin_view_tool/renderer"
+module SachinViewTool
+  # Your code goes here...
+end
---
+++ lib/sachin_view_tool/version.rb
@@ -0,0 +1,3 @@
+module SachinViewTool
+  VERSION = "0.1.0"
+end
